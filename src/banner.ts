4246c822d14b1e4739ddd4936381eff516ad9eef
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-12-18 03:29:15 -0700

    fix: access to package.json (#30)

--- src/banner.ts
+++ src/banner.ts
@@ -1,13 +1,6 @@
 import figlet from 'figlet';
-import fs from 'fs';
-import { join } from 'path';
-
-function loadPkgInfo() {
-    const pkgFile = join(process.cwd(), 'package.json');
-    return JSON.parse(fs.readFileSync(pkgFile).toString());
-}
+import { name, version} from '../package.json';
 
 export default async function banner(): Promise<void> {
-    const { name, version } = loadPkgInfo();
     console.log(`${figlet.textSync(name)} v${version}\n\n`);
 }
